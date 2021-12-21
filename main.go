5ea6c14bc1df4c0bdeb616c918e649d1f2c5d13f
Edgar Talledos <onimenotsuki@gmail.com>
2021-12-21 09:45:18 -0600

    FIX syntax errors

--- main.go
+++ main.go
@@ -29,14 +29,14 @@ func main() {
 		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/#environment-variable")
 	}
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApllyURI(uri))
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 
 	if err != nil {
 		panic(err)
 	}
 
 	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nill {
+		if err := client.Disconnect(context.TODO()); err != nil {
 			panic(err)
 		}
 	}()
